1582fc623bb2a811db44125a7242803e84424707
liaoyuecai <liaoyuecai@163.com>
2018-08-20 13:57:30 +0800

    config

---
+++ backend/quick-cloud/config/src/main/java/org.quick.config/Config.java
@@ -0,0 +1,16 @@
+package org.quick.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class Config {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config.class, args);
+    }
+}
