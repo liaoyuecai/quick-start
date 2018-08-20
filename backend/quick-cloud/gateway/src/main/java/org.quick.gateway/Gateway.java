310153d5138faf63eeb38df30d5d34e93de5471b
liaoyuecai <liaoyuecai@163.com>
2018-08-20 14:55:00 +0800

    网关

---
+++ backend/quick-cloud/gateway/src/main/java/org.quick.gateway/Gateway.java
@@ -0,0 +1,38 @@
+package com.hifo.cloud.gateway;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableZuulProxy
+@SpringCloudApplication
+@EnableDiscoveryClient
+public class Gateway {
+
+
+    @Value("${allowed.origin}")
+    String allowedOrigin;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(allowedOrigin);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class, args);
+    }
+}
